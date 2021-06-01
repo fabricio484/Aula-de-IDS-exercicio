8a043b57f9b19c27b6b685c04e2d0f686184b230
Fabricio <fabricio_soares180@hotmail.com>
2021-05-31 22:34:20 -0300

    refatorado. soma feita com funçao

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,12 @@
+def soma (a, b):
+    return a + b
+
 a = int(input("Digitre o primeiro valor"))
 b = int(input("Digite o segundo valor"))
+
 operacao = input("+: Soma\n-: Subtração\n*: Multiplicaçao\n/: Divisão\n**: Exponenciaçao")
 if operacao == '+':
-    resultado = a + b
+    resultado = soma(a, b)
 elif operacao == '-':
     resultado = a - b
 elif operacao == '*':
