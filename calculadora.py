d560bbffb464252ed3af924f27068a9bce304b11
Fabricio <fabricio_soares180@hotmail.com>
2021-05-31 22:22:11 -0300

    agora faz exponenciaçao

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,14 @@
 a = int(input("Digitre o primeiro valor"))
 b = int(input("Digite o segundo valor"))
-operacao = input("+: Soma\n-: Subtração\n*: Multiplicaçao\n/: Divisão")
+operacao = input("+: Soma\n-: Subtração\n*: Multiplicaçao\n/: Divisão\n**: Exponenciaçao")
 if operacao == '+':
     resultado = a + b
 elif operacao == '-':
     resultado = a - b
 elif operacao == '*':
     resultado = a * b:
-else:
+elif operacao == '/':
     resultado = a // b
+else:
+    resultado = a ** b
 print (resultado)
